da5ab7ef8094699feb32c1e5ef85cbaf41faab8c
Assaf Hillel <68805670+amazingProj@users.noreply.github.com>
2022-07-18 12:39:06 +0300

    l

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,39 +1,71 @@
 import React, { useState } from "react";
 import { MenuIcon, XIcon } from "@heroicons/react/outline";
+import Fullpage, {
+  FullPageSections,
+  FullpageSection,
+  FullpageNavigation,
+} from "@ap.cx/react-fullpage";
+
 import Navbar from "../components/navbar";
+import Chart from "../components/chart";
 const Home = (props) => {
   const [nav, setNav] = useState(false);
 
   const handleClick = () => setNav(!nav);
 
+  const SectionStyle = {
+    height: "100vh",
+    width: "100%",
+  };
+
   return (
     <div className="bg-white h-screen">
-      <Navbar />
+      <Fullpage>
+        <FullPageSections>
+          <FullpageSection style={SectionStyle}>
+            <Navbar />
+
+            <br />
+            <br />
+            <br />
+            <hr id="home" className="border-hidden" />
+
+            <Chart />
+          </FullpageSection>
+          <FullpageSection style={SectionStyle}>
+            <Navbar />
+
+            <br />
+            <br />
+            <br />
+
+            <hr id="withdraws" />
+
+            <p>Withdraws</p>
+          </FullpageSection>
+          <FullpageSection style={SectionStyle}>
+            <hr id="new_account" className="border-hidden" />
+
+            <p>New account</p>
+          </FullpageSection>
+          <FullpageSection style={SectionStyle}>
+            <hr id="transactions" className="border-hidden" />
+
+            <p>Transactions</p>
+          </FullpageSection>
+        </FullPageSections>
+      </Fullpage>
 
       <br />
       <br />
       <br />
       <br />
-      <hr id="home" />
 
-      <p>Home</p>
-      <hr id="withdraws" />
-
-      <p>Withdraws</p>
-
-      <hr id="new_account" />
-
-      <p>New account</p>
-
-      <hr id="transactions" />
-
-      <p>Transactions</p>
-
-      <hr id="transfer" />
+      <hr id="transfer" className="border-hidden" />
 
       <p>Transfer</p>
 
-      <hr id="my_loans" />
+      <hr id="my_loans" className="border-hidden" />
 
       <p>My loans</p>
     </div>
