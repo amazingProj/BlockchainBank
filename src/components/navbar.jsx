839c55a969068485494ea720db0a7dfbdc9c1606
Assaf Hillel <68805670+amazingProj@users.noreply.github.com>
2022-07-18 12:12:36 +0300

    commit

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -7,48 +7,63 @@ const Navbar = (props) => {
   const handleClick = () => setNav(!nav);
 
   return (
-    <div>
-      <div className={!nav ? "hidden" : "mb-6 text-5xl text-blue-700 mr-5"}>
-        LevCoin
-      </div>
-      <div class="relative h-32 w-32 ...">
-        <div class="absolute inset-x-0 top-0 h-16 ...">
-          <div className="w-3/4 h-[60px] z-10 bg-zinc-200 fixed drop-shadow-lg px-8">
-            <div className="px-2 flex justify-between items-center w-full h-full">
-              <div className="flex items-center">
-                <ul className="hidden md:flex text-xl">
-                  {props.isManager && (
-                    <li className="hover:bg-zinc-300 mt-2">Withdraws</li>
-                  )}
+    <div className="w-5/6 h-[60px] z-10 bg-zinc-200 fixed drop-shadow-lg">
+      <div className="flex">
+        <div className="hidden md:block mb-6 text-5xl text-blue-700 mr-5">
+          LevCoin
+        </div>
+        <div class="relative h-32 w-32 ...">
+          <div class="absolute inset-x-0 top-0 h-16 ...">
+            <div className="w-full h-[60px] z-10 bg-zinc-200 fixed drop-shadow-lg px-8">
+              <div className="px-2 flex justify-between items-center w-full h-full">
+                <div className="flex items-center">
+                  <ul className="hidden md:flex text-xl">
+                    {props.isManager && (
+                      <li className="hover:bg-zinc-300 mt-2">Withdraws</li>
+                    )}
 
-                  <li className="hover:bg-zinc-300 mt-2 mr-10">
-                    {" "}
-                    <a href="#home">Home</a>
-                  </li>
-                  <li className="hover:bg-zinc-300 mt-2 mr-10">Withdraws</li>
-                  <li className="hover:bg-zinc-300 mt-2 mr-10">New Account</li>
-                  <li className="hover:bg-zinc-300 mt-2 mr-10">Transactions</li>
-                  <li className="hover:bg-zinc-300 mt-2 mr-10">Transfer</li>
-                  <li className="hover:bg-zinc-300 mt-2 mr-10">My loans</li>
-                </ul>
-                <div className="md:hidden mr-4" onClick={handleClick}>
-                  {!nav ? (
-                    <MenuIcon className="w-5" />
-                  ) : (
-                    <XIcon className="w-5" />
-                  )}
+                    <li className="hover:bg-zinc-300 mt-2 mr-10">
+                      {" "}
+                      <a href="#home">Home</a>
+                    </li>
+                    <li className="hover:bg-zinc-300 mt-2 mr-10">Withdraws</li>
+                    <li className="hover:bg-zinc-300 mt-2 mr-10">
+                      New Account
+                    </li>
+                    <li className="hover:bg-zinc-300 mt-2 mr-10">
+                      Transactions
+                    </li>
+                    <li className="hover:bg-zinc-300 mt-2 mr-10">Transfer</li>
+                    <li className="hover:bg-zinc-300 mt-2 mr-10">My loans</li>
+                  </ul>
+                  <div className="md:hidden mr-4" onClick={handleClick}>
+                    {!nav ? (
+                      <MenuIcon className="w-5" />
+                    ) : (
+                      <XIcon className="w-5" />
+                    )}
+                  </div>
                 </div>
               </div>
             </div>
           </div>
         </div>
-        <div></div>
+        <ul
+          className={
+            !nav ? "hidden" : "absolute mt-20 bg-zinc-200 w-full px-8 mb-5"
+          }
+        >
+          <li className="hover:bg-zinc-300 border-b-2 border-zinc-300 w-full">
+            Home
+          </li>
+          <li className="hover:bg-zinc-300 border-b-2 border-zinc-300 w-full">
+            Withdraws
+          </li>
+          <li className="hover:bg-zinc-300 border-b-2 border-zinc-300 w-full">
+            New Account
+          </li>
+        </ul>
       </div>
-      <ul className={!nav ? "hidden" : "absolute bg-zinc-200 w-full px-8"}>
-        <li className="hover:bg-zinc-300 border-b-2 border-zinc-300 w-full"></li>
-        <li className="hover:bg-zinc-300 border-b-2 border-zinc-300 w-full"></li>
-        <li className="hover:bg-zinc-300 border-b-2 border-zinc-300 w-full"></li>
-      </ul>
     </div>
   );
 };
